6fe138b7ce40d39174f724471ac9ca94385f3563
ElCholoGamer <josedanielgrayson@gmail.com>
2021-02-21 20:08:02 -0500

    this

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import './index.css';
 import Game from './structures/game';
 
-const canvas = document.getElementById('canvas') as HTMLCanvasElement;
+const canvas = document.getElementById('game') as HTMLCanvasElement;
 
 const game = new Game(canvas);
 game.start();
