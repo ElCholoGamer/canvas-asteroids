b7d5895704de89b43d647200d414c871ae53c87e
ElCholoGamer <josedanielgrayson@gmail.com>
2021-02-21 22:30:07 -0500

    add sprite manager and ship sprite

--- src/enviroment.d.ts
+++ src/enviroment.d.ts
@@ -2,3 +2,8 @@ interface Math {
 	radians(degrees: number): number;
 	degrees(radians: number): number;
 }
+
+declare module '*.png' {
+	const url: string;
+	export default url;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
 import './index.css';
 import './math';
 import Game from './structures/game';
+import ShipSprite from './assets/img/ship.png';
 
 const canvas = document.getElementById('game') as HTMLCanvasElement;
 
 const game = new Game(canvas);
-game.start();
+
+(async () => {
+	await game.sprites.load('ship', ShipSprite);
+	game.start();
+})();
